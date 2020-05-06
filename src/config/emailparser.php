30f98f25ee56d26299de3fc9e8224b77d60b8885
Jan Strnadel <jstrnadel@gmail.com>
2020-05-06 08:43:30 +0200

    remove cred

--- src/config/emailparser.php
+++ src/config/emailparser.php
@@ -16,8 +16,8 @@ return [
             'protocol'  => env('IMAP_PROTOCOL', 'imap'), //might also use imap, [pop3 or nntp (untested)]
             'encryption'    => env('IMAP_ENCRYPTION', 'ssl'), // Supported: false, 'ssl', 'tls', 'notls', 'starttls'
             'validate_cert' => env('IMAP_VALIDATE_CERT', true),
-            'username' => env('IMAP_USERNAME', 'yan454@seznam.cz'),
-            'password' => env('IMAP_PASSWORD', 'barunka'),
+            'username' => env('IMAP_USERNAME', ''),
+            'password' => env('IMAP_PASSWORD', ''),
         ]
     ]
 ];
